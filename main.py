21f5a3b2270a2f9c9eacd49ccb1e50f8e0559f77
kcko <kcko2924@gmail.com>
2020-06-22 22:23:16 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import csv
 import pickle
+import logging
 from logger import myLogger
 
 # Declare Logger
-myLogger = myLogger(__name__)
+myLogger = myLogger(__name__, log_level=logging.INFO)
 logger = myLogger.setLogger()
 
 char_load_file = 'characters.csv'
